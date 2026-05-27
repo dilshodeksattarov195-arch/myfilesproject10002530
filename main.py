08f51893a9ef39b84f09308aa96d3411850188a9
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 15:22:39 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class userCrocessWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = userCrocessWorker()
+    print("Worker engine initialized.")
